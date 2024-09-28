8625c1d321a38be9fd02b0fdd9fd90046fc96916
prashant766 <prashant.pathak112@gmail.com>
2024-09-28 14:46:28 +0530

    datatypes

---
+++ datatype.js
@@ -0,0 +1,14 @@
+"use strict" // treat all js code as newer version
+
+// alert (3+3) // we are using node js not browser
+
+// console.log(50+250)
+
+// console.log("priyanshu")
+ let age = 67
+ let namw = "hdhgf"
+ let isloggeIn=false
+ let state;
+
+console.log(typeof age);
+
